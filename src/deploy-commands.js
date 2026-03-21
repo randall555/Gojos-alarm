c4514272a8c8ad4db8651fcc0c40a7a005fdb187
randall555 <suckafatone329@gmail.com>
2026-03-21 02:03:18 -0400

    Add src/deploy-commands.js

---
+++ src/deploy-commands.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if (command.data) {
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log(`Refreshing ${commands.length} application (/) commands.`);
+
+    const data = await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
